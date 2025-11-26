0dc95e2acc9a06a54ca659e13489cf609c8ebb8a
Yofa <mohammadyofaarifin@gmail.com>
2025-11-26 15:17:54 +0700

    menambahkan route API untuk register dan login

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/check', function () {
     return ['status' => true, 'message' => 'API working'];
 });
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
